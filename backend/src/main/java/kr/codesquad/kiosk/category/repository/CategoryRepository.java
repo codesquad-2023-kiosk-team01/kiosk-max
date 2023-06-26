98fcd4fa36192f62e5341ce5995f4a41e914e2a0
Jeongyong Park <1239824@naver.com>
2023-06-26 11:19:43 +0900

    fix : conflict 해결

--- backend/src/main/java/kr/codesquad/kiosk/category/repository/CategoryRepository.java
+++ backend/src/main/java/kr/codesquad/kiosk/category/repository/CategoryRepository.java
@@ -1,22 +1,22 @@
 package kr.codesquad.kiosk.category.repository;
 
-import java.util.Collections;
-import java.util.List;
-
+import kr.codesquad.kiosk.category.domain.Category;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import kr.codesquad.kiosk.category.domain.Category;
-import lombok.RequiredArgsConstructor;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
 public class CategoryRepository {
 	private final NamedParameterJdbcTemplate jdbcTemplate;
-	private final RowMapper<Category> categoryRowMapper = ((rs, rowNum) -> new Category(rs.getInt("id"),
-		rs.getString("name")));
+	private final RowMapper<Category> categoryRowMapper = (rs, rowNum) -> new Category(
+			rs.getInt("id"),
+			rs.getString("name"));
 
 	public List<Category> findAll() {
 		try {
