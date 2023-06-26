6cc9f066be5f7a503e0774cc448ef9dfd276c2a2
jay <chlwodnjs34@gmail.com>
2023-06-26 17:34:17 +0900

    chore #28 : auto deploy 수정

--- frontend/auto-deploy.sh
+++ frontend/auto-deploy.sh
@@ -1,5 +1,5 @@
-ROOT_DIR = “/home/ubuntu/front”
-S3_BUCKET=“s3://kiosk-team01-actions-front-bucket”
+ROOT_DIR = "/home/ubuntu/front"
+S3_BUCKET = "s3://kiosk-team01-actions-front-bucket"
 
 mkdir -p ${ROOT_DIR}
 aws s3 cp ${S3_BUCKET}/ .
