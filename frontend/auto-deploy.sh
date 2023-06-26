f62997e630d056170c09b1fa2ab35be21f5261a8
jay <chlwodnjs34@gmail.com>
2023-06-26 17:49:00 +0900

    chore #28 : auto deploy 수정

--- frontend/auto-deploy.sh
+++ frontend/auto-deploy.sh
@@ -2,6 +2,6 @@ ROOT_DIR = "/home/ubuntu/front"
 S3_BUCKET = "s3://kiosk-team01-actions-front-bucket"
 
 mkdir -p ${ROOT_DIR}
-aws s3 cp ${S3_BUCKET}/ .
+aws s3 cp ${S3_BUCKET} ${ROOT_DIR}/ --recursive
 
 sudo service nginx restart
