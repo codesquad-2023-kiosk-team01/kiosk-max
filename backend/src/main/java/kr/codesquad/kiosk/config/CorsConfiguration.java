4f1c7bead2b9690a411a7c6152ac4438ebc1f354
O_jeegu <lumirite0@gmail.com>
2023-07-01 01:02:27 +0900

    refactor : 서버 연결 테스트

--- backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
+++ backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
@@ -11,6 +11,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
-			.allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "PATCH");
+			.allowedMethods("*");
 	}
 }
