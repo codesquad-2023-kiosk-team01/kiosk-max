e23fa76354e2478f1efcbffbf4a2c32f33a8f214
O_jeegu <lumirite0@gmail.com>
2023-07-01 00:14:36 +0900

    refactor : EC2 연결을 위한 테스트용 리팩토링

--- backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
+++ backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
@@ -4,17 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration(proxyBeanMethods = false)
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://13.124.226.31:80",
-						"http://43.201.168.11:8080",
-						"http://localhost:3000",
-						"http://localhost:8080",
-						"http://127.0.0.1:3000")
-				.allowedMethods("GET", "POST", "OPTION", "PUT");
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "PATCH");
 	}
 }
